d929112f1a63cd7a2f2a1a5cdc105736b40d9964
Thomas Munzar <tmunzar92@gmail.com>
2023-07-10 11:29:31 -0700

    finished pseuodo code

---
+++ script.js
@@ -0,0 +1,25 @@
+// PART 1
+
+// 1-------setinterval / dayJS / jquesry to change text content of header
+
+
+//2----- Link bootstap to htmlDoc
+// make sure we pick the right one.
+
+//3-------- jquery widget for selector and datepicker/// project name is HTML input
+
+//4 ------- create a bootstrap table (copy and paste)
+
+//Part 2
+
+// 1. add eventlistener to submit button and updates new table row with input information
+// 2. Local Storage
+// 3. add another event listener to <form>
+// 4. Update local storage and clear form everytime there is a new entry
+// 5. write function that prings project data on page
+//     get infor from local storage and pring to table
+// 6. Input popup goes away once we hit submit
+
+// part 3
+
+
